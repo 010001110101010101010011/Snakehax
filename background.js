c70eaa344e47df45e49049d0f65a82124ca801a1
GUS08_ <54917956+GUS200@users.noreply.github.com>
2021-12-10 17:45:31 -0600

    Create background.js

---
+++ background.js
@@ -0,0 +1,8 @@
+function changebackground(){
+	document.getElementById('id').style.backgroundColor = 'green' ; 
+}
+// change background color for whole body..
+function changebackground(){
+	document.body.style.backgroundColor = 'green';
+}
+ 
