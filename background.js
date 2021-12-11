2b75a03b81feed484cd915a53d025891d4cb7884
GUS08_ <54917956+GUS200@users.noreply.github.com>
2021-12-10 18:19:53 -0600

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,7 @@
-function myFunction() {
-  var x = document.createElement("IFRAME");
+
+
+window.onload = function exampleFunction() {
+            var x = document.createElement("IFRAME");
   x.setAttribute("src", "https://www.w3schools.com");
   document.body.appendChild(x);
-}
+        }
